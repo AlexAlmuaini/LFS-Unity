007f9ad95cd1c8804cb62c46e063f396f55eec6e
Jake <jake2.Webster@live.uwe.ac.uk>
2023-04-11 19:23:27 +0100

    some aesthetic things

--- Assets/Scripts/Scene Tinkering/globalStuff.cs
+++ Assets/Scripts/Scene Tinkering/globalStuff.cs
@@ -7,4 +7,7 @@ public class globalStuff : MonoBehaviour
 {
     public static int door_number;
     public static bool door_passed;
+    public static bool torch_collected;
+    public static bool wire_collected;
+    public static bool tnt_collected;
 }
