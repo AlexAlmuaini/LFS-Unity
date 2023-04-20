5c7fcd0cd3347f6a5a2a625db534de44766202cf
Thomas Allison <Thomas2.Allison@live.uwe.ac.uk>
2023-04-20 12:03:56 +0100

    Added fence puzzle
    added the basis for a fence puzzle in the barn scene

--- Assets/Scripts/FencePuzzle.cs
+++ Assets/Scripts/FencePuzzle.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class FencePuzzle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject playerCamera;
+    public GameObject CutsceneCamera;
+    public GameObject CutsceneCamera2;
+    public GameObject timeline;
+    public Rigidbody rb;
+    public GameObject plank;
+    public MeshRenderer rend;
+    public bool startCutscene;
+    
+    void Awake()
     {
-        
+        CutsceneCamera.SetActive(false);
+        CutsceneCamera2.SetActive(false);
+        timeline.SetActive(false);
+        playerCamera.SetActive(true);
+        rend = plank.GetComponent<MeshRenderer>();
+        rb = GameObject.Find("RPG-Character Variant").GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FinishCut()
     {
-        
+        yield return new WaitForSeconds(7);
+        playerCamera.SetActive(true);
+        CutsceneCamera.SetActive(false);
+        CutsceneCamera2.SetActive(false);
+        timeline.SetActive(false);
+        gameObject.SetActive(false);
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        if (startCutscene)
+        {
+            playerCamera.SetActive(false);
+            timeline.SetActive(true);
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            rend.enabled = false;
+            StartCoroutine(FinishCut());
+           
+
+
+        }
     }
 }
