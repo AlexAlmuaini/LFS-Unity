58fc0c3586f2012da5a59f2b75e2ab191d0e9c3b
Alexander Al-Muaini <alexander2.al-muaini@live.uwe.ac.uk>
2023-04-29 13:37:36 +0100

    martyn boss logic added
     boss now groundpounds and dashes depending on how close the player is

--- Assets/Alex's Assets/Player/Scripts/KingSlimeBehaviour.cs
+++ Assets/Alex's Assets/Player/Scripts/KingSlimeBehaviour.cs
@@ -13,7 +13,7 @@ public class KingSlimeBehaviour : MonoBehaviour
     private Transform player;
     [SerializeField] GameObject deathParticles;
     private float dist, flashTime = 0.15f, deathTime = 5.0f;
-    public float moveSpeed, health = 8;
+    public float moveSpeed, health = 25;
     public float detectionRad;
     public float damping;
     public bool isMoving;
@@ -23,6 +23,23 @@ public class KingSlimeBehaviour : MonoBehaviour
     private bool onGround;
     private float timer;
     private bool returnState;
+    
+   public float cooldown = 5.0f;
+
+    private bool canAttack = true;
+    private float attackTimer = 0;
+
+    public float groundPoundRadius = 5.0f;
+    public float groundPoundDamage = 10.0f;
+    public float coneAngle = 90.0f;
+    public float coneDistance = 20.0f;
+
+    public float chargeSpeed = 10f;
+    public float chargeDuration = 2f;
+
+    private bool isCharging = false;
+    private bool isRotating = false;
+    private float rotationTimer;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,10 +59,31 @@ public class KingSlimeBehaviour : MonoBehaviour
             ReturningToPost();
         }
         else
-        {
+        {  
             HandleMovement();
         }
         HealthStates();
+        
+        //Attack cooldown
+        if (!canAttack)
+        {
+            attackTimer += Time.deltaTime;
+            if (attackTimer >= cooldown)
+            {
+                canAttack = true;
+                attackTimer = 0.0f;
+            }
+        }
+        if (isRotating)
+        {
+            rotationTimer += Time.deltaTime;
+            RotateTowardsTarget(rotationTimer);
+            
+        }
+        if (isCharging)
+        {
+            Charge();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -70,38 +108,40 @@ public class KingSlimeBehaviour : MonoBehaviour
     private void HandleMovement()
     {
         dist = Vector3.Distance(player.position, transform.position);
-        
-        if(dist <= detectionRad)
+        if(!isCharging)
         {
-            var lookPos = player.position - transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
-            GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed);
-            isMoving = true;
-        }
-        else
-        {
-            var lookPos = patrolPost.position - transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
-            GetComponent<Rigidbody>().AddForce(transform.forward * 750);
-            isMoving = false;
-            if (onGround)
+            if(dist <= detectionRad)
             {
-                timer += Time.deltaTime;
+                var lookPos = player.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+                GetComponent<Rigidbody>().AddForce(transform.forward * 1200);
+                isMoving = true;
             }
-        }
+            else
+            {
+                var lookPos = patrolPost.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+                GetComponent<Rigidbody>().AddForce(transform.forward * 1200);
+                isMoving = false;
+                if (onGround)
+                {
+                    timer += Time.deltaTime;
+                }
+            }
+        }   
     }
 
     private void OnCollisionStay(Collision col)
    {
-    if(col.gameObject.tag == "Floor")
+        if(col.gameObject.tag == "Floor")
         {
             if(isMoving == true)
             {
-                GetComponent<Rigidbody>().AddForce(transform.up * 30000);
+                GetComponent<Rigidbody>().AddForce(transform.up * 75000);
             }
 
             if (isMoving == false)
@@ -111,7 +151,7 @@ public class KingSlimeBehaviour : MonoBehaviour
                 {
                     onGround = false;
                     timer = 0;
-                    GetComponent<Rigidbody>().AddForce(transform.up * 30000 * 5);
+                    GetComponent<Rigidbody>().AddForce(transform.up * 75000 * 5);
                 }
             }
         }
@@ -121,8 +161,8 @@ public class KingSlimeBehaviour : MonoBehaviour
             if(playerMovement.attacking)
             {
                 StartCoroutine(EFlash());
-                GetComponent<Rigidbody>().AddForce(transform.up * 20000);
-                GetComponent<Rigidbody>().AddForce(transform.forward * -30000);
+                GetComponent<Rigidbody>().AddForce(transform.up * 40000);
+                GetComponent<Rigidbody>().AddForce(transform.forward * -60000);
                 health--;
             }
         }     
@@ -135,6 +175,7 @@ public class KingSlimeBehaviour : MonoBehaviour
             StartCoroutine(EDeath());
         }
    }
+
    IEnumerator EFlash()
    {
     meshRenderer.material.color = damageColor;
@@ -154,4 +195,108 @@ public class KingSlimeBehaviour : MonoBehaviour
     yield return new WaitForSeconds(deathTime);
     Destroy(deadSlime);
    }
+
+
+    void FixedUpdate()
+    {
+        if (canAttack)
+        {
+            canAttack = false;
+            //Temporary random attack choice for testing
+            // will be based on player position in relation to boss
+            int num = Random.Range(0, 2);
+            //print("Cooldown over, attack selected:" + num);
+            num = 1;
+            if (num == 0)
+            {
+                groundPound();
+            }
+            else if (num == 1 && dist >= detectionRad/3)
+            {
+                isRotating = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isCharging && collision.gameObject.CompareTag("Player"))
+        {
+            // deal damage to player and stop charging sequence
+            //collision.GetComponent<PlayerController>().TakeDamage(attackDamage);
+            isCharging = false;
+            playerMovement.GetComponent<Rigidbody>().AddForce(transform.up * 3000);
+            playerMovement.GetComponent<Rigidbody>().AddForce(transform.forward * 12000);
+        }
+    }
+
+    void groundPound()
+    {
+        // Get boss's forward vector and calculate cone bounds
+        Vector3 bossForward = transform.forward;
+        Vector3 coneOrigin = transform.position + Vector3.up; // Add offset to avoid detecting boss's own collider
+        float coneHalfAngle = coneAngle / 2.0f;
+        float coneHalfRadians = coneHalfAngle * Mathf.Deg2Rad;
+        float coneDistanceSquared = coneDistance * coneDistance;
+        Vector3 coneRight = Quaternion.AngleAxis(coneHalfAngle, Vector3.up) * bossForward;
+        Vector3 coneLeft = Quaternion.AngleAxis(-coneHalfAngle, Vector3.up) * bossForward;
+
+        // Detect all colliders within cone shape in front of boss
+        Collider[] hitColliders = Physics.OverlapSphere(coneOrigin, coneDistance);
+        foreach (Collider hitCollider in hitColliders)
+        {
+            //print("Collision");
+            if (hitCollider.tag == "Player")
+            {
+                print("Player Collision");
+                // Check if player character is within cone angle and distance
+                Vector3 toPlayer = hitCollider.transform.position - coneOrigin;
+                print("to player vector" + toPlayer);
+                float dotRight = Vector3.Dot(toPlayer.normalized, coneRight);
+                float dotLeft = Vector3.Dot(toPlayer.normalized, coneLeft);
+
+                if (toPlayer.magnitude <= coneDistance)
+                {
+                    // detected in circle
+                    //hitCollider.GetComponent<PlayerController>().TakeDamage(attackDamage);
+    
+                    if (dotRight > Mathf.Cos(coneHalfRadians) && dotLeft < Mathf.Cos(coneHalfRadians))
+                    {
+                        // buggy but detects in front
+                        print("in Front");
+                    }
+                }
+            }
+        }
+    }
+
+    void Charge()
+    {
+        Vector3 direction = Vector3.forward;
+        direction.y = 0;
+        GetComponent<Rigidbody>().AddForce(transform.forward * 7500);
+    }
+
+    void RotateTowardsTarget(float timer)
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime);
+        if (Quaternion.Angle(transform.rotation, rotation) < 2f || timer > 5 )
+        {
+            isRotating = false;
+            isCharging = true;
+            transform.rotation = rotation;
+            StartCoroutine(StopChargingAfterDelay());
+        }
+
+
+    }
+
+    IEnumerator StopChargingAfterDelay()
+    {
+        yield return new WaitForSeconds(chargeDuration);
+        isCharging = false;
+    }
 }
