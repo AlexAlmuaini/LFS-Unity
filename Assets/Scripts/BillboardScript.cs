c32b617bbe0bb57dea5cf6b556775cef0f730929
Thomas Allison <Thomas2.Allison@live.uwe.ac.uk>
2023-05-04 11:02:41 +0100

    Made the health bar follow the boss
    Also fixed some minor errors by switching the event system to the new input system.

--- Assets/Scripts/BillboardScript.cs
+++ Assets/Scripts/BillboardScript.cs
@@ -5,34 +5,9 @@ using UnityEngine;
 public class BillboardScript : MonoBehaviour
 {
     public Transform cam;
-    ButtonBehaviourScript button;
-    Canvas canvas;
 
-    private void Awake()
-    {
-        button = FindObjectOfType<ButtonBehaviourScript>();
-        canvas = GetComponent<Canvas>();
-        canvas.enabled = false;
-    }
     void LateUpdate()
     {
         transform.LookAt(transform.position + cam.forward);
-    }
-
-    private void Update()
-    {
-        if(button.canPressButton)
-        {
-            canvas.enabled = true;
-        }
-        else
-        {
-            canvas.enabled = false;
-        }
-
-        if (button.buttonPressed)
-        {
-            Destroy(this.gameObject);
-        }
     }
 }
